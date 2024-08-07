48041ee91df9379967cf9e94a2232ca59b623cce
vosw1 <vosw1@naver.com>
2024-08-07 13:16:19 +0900

    inoutinstruction 페이지 기본 디자인

--- lib/page/in_out_industruction_page/in_out_industruction_page.dart
+++ lib/page/in_out_industruction_page/in_out_industruction_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:the_boxes/page/home/components/custom_app_bar.dart';
 
+import 'components/in_out_instruction_body.dart';
+
 class InOutInstructionPage extends StatelessWidget {
   const InOutInstructionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: '입출고'),
+      appBar: CustomAppBar(title: '입출고 지시'),
+      body: InOutInstructionBody(),
     );
   }
 }
+
